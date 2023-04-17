e4779eb9192e547c54b20ae232c78fc948bbd1b7
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-17 16:57:35 +0800

    Learn about lists

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Console con = System.console();
+        List<String> todos = new ArrayList<>(); // Unlike Arrays, lists can store as many items as you want
+        String input = "";
+
+        while (!input.equals("quit")) {
+            input = con.readLine("Enter a new todo: ");
+            if (!input.equals("quit")) {
+                todos.add(input);
+            }
+        }
+        
+        for (String todo: todos) {
+            System.out.println("Todo task: " + todo);
+        }
     }
 }
